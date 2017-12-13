544e4c92789082e1594b777acd5c6180cf038965
Valentina Rodrigues <valentinarodrigues93@gmail.com>
2017-12-13 19:39:55 +0530

    Add Assignment 5/6

--- GradientPrac.py
+++ GradientPrac.py
@@ -19,13 +19,7 @@ y=np.array([1,1,1,1,1,1,1,1,1])
 w=np.linspace(-1, 1, 6)
 X = expand(X1)
 
-
-
 def compute_grad(w, X, y):
-    """
-    Given feature matrix X [n_samples,6], target vector [n_samples] of 1/0,
-    and weight vector w [6], compute vector [6] of derivatives of L over each weights.
-    """    
     def gradient(w, X, y):
         i=np.dot((nn(X, w) - y), X)
         return i
@@ -41,24 +35,4 @@ def compute_grad(w, X, y):
     l=gradient(w, X, y)
     return l
 print(compute_grad(w, X, y))
-"""def gradient(w, x, t): 
-    return print(np.dot((nn(x, w) - t), x))
 
-# Define the logistic function
-def logistic(z): 
-    return 1 / (1 + np.exp(-z))
-
-# Define the neural network function y = 1 / (1 + numpy.exp(-x*w))
-def nn(x, w): 
-    return logistic(x.dot(w.T))
-
-X1 = np.array([[ 1.20798057,  0.0844994 ],[ 1.20798057,  0.0844994 ],
-               [ 1.20798057,  0.0844994 ],[ 1.20798057,  0.0844994 ],
-[ 1.20798057,  0.0844994 ],[ 1.20798057,  0.0844994 ],
- [ 0.76121787,  0.72510869], [ 0.76121787,  0.72510869],
- [ 0.55256189,  0.51937292]])
-y=np.array([1,1,1,1,1,1,1,1,1])
-w=np.linspace(-1, 1, 6)
-X = expand(X1)
-gradient(w, X, y)
-"""
--- Week01.py
+++ Week01.py
@@ -41,7 +41,76 @@ def probability(X, w):
     # TODO:<your code here>
 
 
+def compute_loss(X, y, w):
+    """
+    Given feature matrix X [n_samples,6], target vector [n_samples] of 1/0,
+    and weight vector w [6], compute scalar loss function using formula above.
+    """
+    pi1=np.zeros((y.shape))
+    pi2=np.zeros((y.shape))
+    pi1=probability(X,w)
+    for j,t in enumerate(y):
+        pi2[j]=np.multiply(t,(np.log(pi1[j]))) + np.multiply((1-t),(np.log(1-pi1[j])))
+    l=-(np.sum(pi2))/y.shape
+    return l
+    # TODO:<your code here>
+
+def compute_grad(X, y, w):
+    """
+    Given feature matrix X [n_samples,6], target vector [n_samples] of 1/0,
+    and weight vector w [6], compute vector [6] of derivatives of L over each weights.
+    """    
+    i=np.dot((probability(X, w) - y),X) after manually solving derivative
+    n=y.shape
+    l=i/n #divide by number of samples
+    return l
     
+    # TODO<your code here>
+
+from IPython import display
+
+h = 0.01
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+def visualize(X, y, w, history):
+    """draws classifier prediction with matplotlib magic"""
+    Z = probability(expand(np.c_[xx.ravel(), yy.ravel()]), w)
+    Z = Z.reshape(xx.shape)
+    plt.subplot(1, 2, 1)
+    plt.contourf(xx, yy, Z, alpha=0.8)
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Paired)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+    
+    plt.subplot(1, 2, 2)
+    plt.plot(history)
+    plt.grid()
+    ymin, ymax = plt.ylim()
+    plt.ylim(0, ymax)
+    display.clear_output(wait=True)
+    plt.show()
+# please use np.random.seed(42), eta=0.1, n_iter=100 and batch_size=4 for deterministic results
+
+np.random.seed(42)
+w = np.array([0, 0, 0, 0, 0, 1])
+
+eta= 0.1 # learning rate
+
+n_iter = 100
+batch_size = 4
+loss = np.zeros(n_iter)
+plt.figure(figsize=(12, 5))
+
+for i in range(n_iter):
+    ind = np.random.choice(X_expanded.shape[0], batch_size)
+    loss[i] = compute_loss(X_expanded, y, w)
+    if i % 10 == 0:
+        visualize(X_expanded[ind, :], y[ind], w, loss)
+    w=w-(eta*compute_grad(X_expanded, y, w))#dont divide by number of samples since already divided
+visualize(X, y, w, loss)
+plt.clf()
 
 
 
